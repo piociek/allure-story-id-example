2290754fdc3479d5e6fb1f21da6e90b849d64cd6
Piotr Michalski <pio.michalski@gmail.com>
2015-12-18 14:14:53 +0100

    Initial commit

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,28 @@
+import org.testng.annotations.Test;
+import ru.yandex.qatools.allure.annotations.Issue;
+import ru.yandex.qatools.allure.annotations.Stories;
+import ru.yandex.qatools.allure.annotations.TestCaseId;
+
+import static org.testng.Assert.assertTrue;
+
+public class DummyTest {
+    @Stories(value = {"story1", "story2"}, id = "12")
+    @TestCaseId("123")
+    @Test
+    public void simpleTestWithStoryAndId(){
+        assertTrue(true);
+    }
+
+    @Stories(value = {"story1", "story3"})
+    @TestCaseId("124")
+    @Test
+    public void simpleTestWithStoryWithoutId(){
+        assertTrue(true);
+    }
+
+    @Issue("321")
+    @Test
+    public void simpleTestWithoutStory(){
+        assertTrue(true);
+    }
+}
